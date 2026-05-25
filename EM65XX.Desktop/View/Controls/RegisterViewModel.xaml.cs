0d2d95e8f7fd7eb19ac87f4c010e8ee8e6772ad3
Artem Grigoryan <artem.grigoryan.j@gmail.com>
2026-05-25 23:47:28 +0300

    Add register control

---
+++ EM65XX.Desktop/View/Controls/RegisterViewModel.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EM65XX.Desktop.View.Controls;
+/// <summary>
+/// Interaction logic for RegisterViewModel.xaml
+/// </summary>
+public partial class RegisterViewModel : UserControl
+{
+    public RegisterViewModel()
+    {
+        InitializeComponent();
+    }
+}
